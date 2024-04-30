51aaf62d74ed273ea5ba4a04c95c96113ecc6fbc
Mohammed-Ismaiel <mohammedismaiel333.z@gmail.com>
2024-04-30 15:22:51 +0300

    init commit

---
+++ src/main/java/com/tobeupdated/jpatriggers/JpatriggersApplication.java
@@ -0,0 +1,13 @@
+package com.tobeupdated.jpatriggers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatriggersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatriggersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobeupdated/jpatriggers/JpatriggersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeupdated.jpatriggers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatriggersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
