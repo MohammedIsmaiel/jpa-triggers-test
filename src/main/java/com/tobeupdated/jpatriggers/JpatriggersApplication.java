8d6ea7c75d6a4f9055acc7eb01e6b5b5c88b92b8
Mohammed-Ismaiel <mohammedismaiel333.z@gmail.com>
2024-05-01 11:19:00 +0300

    added @OpenAPIDefinition

--- src/main/java/com/tobeupdated/jpatriggers/JpatriggersApplication.java
+++ src/main/java/com/tobeupdated/jpatriggers/JpatriggersApplication.java
@@ -3,7 +3,12 @@ package com.tobeupdated.jpatriggers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "jpa with listeners test", version = "1.0"))
+
 public class JpatriggersApplication {
 
 	public static void main(String[] args) {
